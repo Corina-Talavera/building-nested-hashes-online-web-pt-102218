66e3e8254e417d20092941ff8366cc56f77c0f83
Corina Talavera <corina.talavera@gmail.com>
2018-11-02 23:11:16 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,17 +7,10 @@ def second_challenge
       :hero_friends => [
         {
          :name => "Benvolio",
-         :age => "17",
-         :attitude => "worried"
-      },
-      {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-      }
+         },
+      {:name => "Mercutio"}
     ]
-  },]
-   },
+  },
    :capulet => {
       :patriarch => {},
       :matriarch => {},
