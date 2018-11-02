bdaf49c5b7e4f12fe5395534c6be130bd62aad14
Corina Talavera <corina.talavera@gmail.com>
2018-11-02 23:09:33 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {:name => "Lord Montague"},
-      :matriarch => {},
+      :matriarch => {:name => "Lady Montague"},
       :hero => {},
       :hero_friends => []
    },
