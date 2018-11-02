1ddd9e675495f7d962986c4385b09c95e0d75e5c
Corina Talavera <corina.talavera@gmail.com>
2018-11-02 23:12:30 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,7 +10,7 @@ def second_challenge
     ]
   },
    :capulet => {
-      :patriarch => {},
+      :patriarch => {:name => "Lord Capulet"},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
