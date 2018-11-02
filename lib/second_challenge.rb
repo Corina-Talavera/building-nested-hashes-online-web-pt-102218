719e279ec08a060abcbacefea08c24ac3a3fab09
Corina Talavera <corina.talavera@gmail.com>
2018-11-02 23:20:13 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -17,12 +17,10 @@ epic_tragedy = {
       {
          :name => "Benvolio",
          :age => "17",
-         :attitude => "worried"
       },
       {
         :name => "Mercutio",
         :age => "18",
-        :attitude => "hot-headed"
       }
     ]
   },
@@ -38,18 +36,15 @@ epic_tragedy = {
     :heroine => {
       :name => "Juliet",
       :age => "15",
-      :status => "alive"
     },
     :heroine_friends=> [
       {
         :name => "Steven",
         :age => "30",
-        :attitude => "confused"
       },
       {
         :name => "Nurse",
         :age => "44",
-        :attitude => "worried"
       }
     ]
   }
