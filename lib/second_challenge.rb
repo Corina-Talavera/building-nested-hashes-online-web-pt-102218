a6ef7e57c258ae0c0a94624a7c64f30abc5702a0
Corina Talavera <corina.talavera@gmail.com>
2018-11-02 23:25:11 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ eepic_tragedy = {
       :patriarch => {:name => "Lord Montague",},
       :matriarch => {:name => "Lady Montague",},
       :hero => {:name => "Romeo",},
-      :hero_friends => [{:name => "Benvolio",},{:name => }]
+      :hero_friends => [{:name => "Benvolio",},{:name => "Mercutio",}]
    },
    :capulet => {
       :patriarch => {},
